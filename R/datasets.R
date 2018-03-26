15a964f7666b88a8851211779927ede8d85e7ac9
Gregor Sturm <mail@gregor-sturm.de>
2018-03-26 16:01:17 +0200

    Add example data for documentation

---
+++ R/datasets.R
@@ -0,0 +1,6 @@
+#' example gene expression dataset 
+#' 
+#' @format a gene expression matrix with gene_symbols as rownames and sample identifiers as colnames. 
+#' 
+#' TODO describe how it was generated from fantom5 data or use a better dataset instead
+"example_gene_expression_matrix"

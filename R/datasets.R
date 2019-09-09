4487c92b6b7bec63eb166ec3e3717ba0d75e675c
Gregor Sturm <mail@gregor-sturm.de>
2019-09-09 09:11:21 +0200

    Better example dataset (Fix #20)

--- R/datasets.R
+++ R/datasets.R
@@ -1,6 +1,19 @@
-#' example gene expression dataset 
-#' 
-#' @format a gene expression matrix with gene_symbols as rownames and sample identifiers as colnames. 
-#' 
-#' TODO describe how it was generated from fantom5 data or use a better dataset instead
-"example_gene_expression_matrix"
+#' Example RNA-seq dataset from the EPIC publication.
+#
+#' A dataset of four RNA-seq samples from patients with metastatic melanoma.
+#'
+#' @format a gene expression matrix with gene_symbols as rownames and sample identifiers as colnames.
+#'
+#' @source Racle et al. (2017), eLIFE, @url{https://doi.org/10.7554/eLife.26476.029}
+"dataset_racle$expr_mat"
+
+
+#' Immune cell proportions from the Racle-dataset measured with FACS.
+#
+#' Immune cell ccontents of four samples from patients with metastatic melanoma profiled
+#' with Fluorescence activated cell sorting (FACS).
+#'
+#' @format a data.frame with three columns: sample, cell_type, true_fraction
+#'
+#' @source Racle et al. (2017), eLIFE, @url{https://doi.org/10.7554/eLife.26476.029}
+"dataset_racle$ref"

8825d4f0df62d7a02575bd8153218229783d1ff6
Gregor Sturm <mail@gregor-sturm.de>
2019-09-09 09:30:46 +0200

    fix dataset docs

--- R/datasets.R
+++ R/datasets.R
@@ -1,19 +1,11 @@
 #' Example RNA-seq dataset from the EPIC publication.
-#
-#' A dataset of four RNA-seq samples from patients with metastatic melanoma.
+#' 
+#' A dataset of four RNA-seq samples from patients with metastatic melanoma and associated
+#' 'gold standard' immune cell estimates profiled with fluorescence activated cell sorting (FACS)
 #'
-#' @format a gene expression matrix with gene_symbols as rownames and sample identifiers as colnames.
+#' @format an environment with two objects: 
+#'   (1) `expr_mat`: gene expression matrix with gene_symbols as rownames and sample identifiers as colnames.
+#'   (2) `ref`: FACS measurements in a data.frame with three columns: sample, cell_type, true_fraction
 #'
-#' @source Racle et al. (2017), eLIFE, @url{https://doi.org/10.7554/eLife.26476.029}
-"dataset_racle$expr_mat"
-
-
-#' Immune cell proportions from the Racle-dataset measured with FACS.
-#
-#' Immune cell ccontents of four samples from patients with metastatic melanoma profiled
-#' with Fluorescence activated cell sorting (FACS).
-#'
-#' @format a data.frame with three columns: sample, cell_type, true_fraction
-#'
-#' @source Racle et al. (2017), eLIFE, @url{https://doi.org/10.7554/eLife.26476.029}
-"dataset_racle$ref"
+#' @source Racle et al. (2017), eLIFE, https://doi.org/10.7554/eLife.26476.029
+"dataset_racle"
